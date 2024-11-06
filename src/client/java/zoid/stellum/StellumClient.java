cf876de9810837b7d86680f96d2724eeae86ab15
Zoid <108158093+TheZoidMaster@users.noreply.github.com>
2024-11-06 14:32:35 -0600

    added mod template

---
+++ src/client/java/zoid/stellum/StellumClient.java
@@ -0,0 +1,10 @@
+package zoid.stellum;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StellumClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
